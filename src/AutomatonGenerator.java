b087578866444a46f9e1e658e41d8c0e632b6076
damian <d.caprarius@gmail.com>
2024-07-12 22:01:43 +0200

    calculate and test first sets

--- src/AutomatonGenerator.java
+++ src/AutomatonGenerator.java
@@ -92,7 +92,199 @@ public class AutomatonGenerator {
 
     // assumes that createEmpty has already been called
     private void createFirst(Regex r){
-        // TODO
+
+        if(r instanceof Regex.Empty){
+
+            // TODO: what to do here
+            throw new RuntimeException("Not implemented yet.");
+        }
+        else if(r instanceof Regex.Letter){
+
+            int id = rToId.get(r);
+            first.put(id, new HashSet<>(Set.of(id)));
+        }
+        else if(r instanceof Regex.Cat cat){
+
+            createFirst(cat.left);
+            createFirst(cat.right);
+
+            int id = rToId.get(r);
+
+            Set<Integer> firstForR = new HashSet<>(getFirst(cat.left));
+
+            // add second if first one can be empty
+            if(isEmpty(cat.left)) {
+                firstForR.addAll(getFirst(cat.right));
+            }
+
+            first.put(id, firstForR);
+        }
+        else if(r instanceof Regex.Or or){
+
+            createFirst(or.left);
+            createFirst(or.right);
+
+            // union the first sets
+            Set<Integer> firstForR = new HashSet<>(getFirst(or.left));
+            firstForR.addAll(getFirst(or.right));
+
+            first.put(rToId.get(r), firstForR);
+        }
+        else if(r instanceof Regex.Star star){
+
+            createFirst(star.r);
+
+            // pass the first set to the parent
+            Set<Integer> firstForR = new HashSet<>(getFirst(star.r));
+
+            first.put(rToId.get(r), firstForR);
+        }
+        else if(r instanceof Regex.Grouping grouping){
+            createFirst(grouping.r);
+
+            // pass the first set to the parent
+            Set<Integer> firstForR = new HashSet<>(getFirst(grouping.r));
+
+            first.put(rToId.get(r), firstForR);
+        }
+        else {
+            throw new RuntimeException("Unknown regex type");
+        }
+    }
+
+    private void createLast(Regex r){
+        if(r instanceof Regex.Empty){
+
+            // TODO: what to do here
+            throw new RuntimeException("Not implemented yet.");
+        }
+        else if(r instanceof Regex.Letter){
+
+            int id = rToId.get(r);
+            last.put(id, new HashSet<>(Set.of(id)));
+        }
+        else if(r instanceof Regex.Cat cat){
+
+            createLast(cat.left);
+            createLast(cat.right);
+
+            int id = rToId.get(r);
+
+            Set<Integer> lastForR = new HashSet<>(getLast(cat.right));
+
+            // if second one can be empty, first one can be last
+            if(isEmpty(cat.right)) {
+                lastForR.addAll(getLast(cat.left));
+            }
+
+            last.put(id, lastForR);
+        }
+        else if(r instanceof Regex.Or or){
+
+            createLast(or.left);
+            createLast(or.right);
+
+            // union the first sets
+            Set<Integer> lastForR = new HashSet<>(getLast(or.left));
+            lastForR.addAll(getLast(or.right));
+
+            last.put(rToId.get(r), lastForR);
+        }
+        else if(r instanceof Regex.Star star){
+
+            createLast(star.r);
+
+            // pass the first set to the parent
+            Set<Integer> lastForR = new HashSet<>(getLast(star.r));
+
+            last.put(rToId.get(r), lastForR);
+        }
+        else if(r instanceof Regex.Grouping grouping){
+            createLast(grouping.r);
+
+            // pass the first set to the parent
+            Set<Integer> lastForR = new HashSet<>(getLast(grouping.r));
+
+            last.put(rToId.get(r), lastForR);
+        }
+        else {
+            throw new RuntimeException("Unknown regex type");
+        }
+    }
+
+    // needs first set and needs empty attribute
+    // assumes that both have been called beforehand
+
+    // the parent always sets attribute of his children
+    // PRE order traversal
+    private void createNext(Regex r){
+        if(r instanceof Regex.Empty){
+
+            // TODO: what to do here
+            throw new RuntimeException("Not implemented yet.");
+        }
+        else if(r instanceof Regex.Letter){
+            // parent has to set this
+            // if this is the root,
+            // next is initialized to the empty set
+        }
+        else if(r instanceof Regex.Cat cat){
+
+            // next[r] is already initialized
+
+            Set<Integer> nextForRight = new HashSet<>(getNext(r));
+            Set<Integer> nextForLeft = new HashSet<>(getFirst(cat.right));
+
+            // left can pass through right, so next could also be next of right
+            if(isEmpty(cat.right)){
+                nextForLeft.addAll(getNext(r));
+            }
+
+            // insert so that child calls can use them
+            next.put(rToId.get(cat.left), nextForLeft);
+            next.put(rToId.get(cat.right), nextForRight);
+
+            createNext(cat.left);
+            createNext(cat.right);
+        }
+        else if(r instanceof Regex.Or or){
+
+            // next[r] is already initialized
+
+            Set<Integer> nextForRight = new HashSet<>(getNext(r));
+            Set<Integer> nextForLeft = new HashSet<>(getNext(r));
+
+            // insert so that child calls can use them
+            next.put(rToId.get(or.left), nextForLeft);
+            next.put(rToId.get(or.right), nextForRight);
+
+            createNext(or.left);
+            createNext(or.right);
+        }
+        else if(r instanceof Regex.Star star){
+
+            Set<Integer> nextForR = new HashSet<>(getNext(r));
+
+            // could repeat it again so first set of itself?
+            nextForR.addAll(getFirst(star.r));
+
+            // child set so continue
+            next.put(rToId.get(star.r), nextForR);
+
+            createNext(star.r);
+        }
+        else if(r instanceof Regex.Grouping grouping){
+
+            Set<Integer> nextForR = new HashSet<>(getNext(r));
+
+            // child set so continue
+            next.put(rToId.get(grouping.r), nextForR);
+
+            createNext(grouping.r);
+        }
+        else {
+            throw new RuntimeException("Unknown regex type");
+        }
     }
 
     public boolean isEmpty(Regex r){
@@ -100,7 +292,15 @@ public class AutomatonGenerator {
     }
 
     public Set<Integer> getFirst(Regex r){
-        return null;
+        return first.get(rToId.get(r));
+    }
+
+    public Set<Integer> getNext(Regex r){
+        return next.get(rToId.get(r));
+    }
+
+    public Set<Integer> getLast(Regex r){
+        return last.get(rToId.get(r));
     }
 
     public NFA generatorAutomaton(Regex r){
@@ -110,6 +310,14 @@ public class AutomatonGenerator {
         createEmpty(r);
 
         createFirst(r);
+
+        // we always assume that before the call to createNext(r)
+        // its own next value has been set by its parent
+        // start with the empty set because nothing comes after r
+        next.put(rToId.get(r), new HashSet<>());
+        createNext(r);
+
+        createLast(r);
 
         return null;
     }
--- src/AutomatonGeneratorTest.java
+++ src/AutomatonGeneratorTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +23,7 @@ public class AutomatonGeneratorTest {
         Regex aOrb = new Regex.Or(a, b);
 
         Regex aOrBStar = new Regex.Star(new Regex.Grouping(aOrb));
+        Regex aCatAOrB = new Regex.Cat(a, new Regex.Grouping(aOrb));
 
         List<Regex> regexInTree = Utils.flatten(r);
 
@@ -38,6 +39,78 @@ public class AutomatonGeneratorTest {
             else if(regex.isSemanticallyEqual(aOrBStar)) {
                 assertTrue(generator.isEmpty(regex));
             }
+        }
+
+        // at least the sizes of the first sets must be the same
+        // we don't know what the ids mean
+        Map<Regex, Integer> expectedFirstSetSize = new HashMap<>();
+        expectedFirstSetSize.put(r, 3);
+        expectedFirstSetSize.put(aOrb, 2);
+        expectedFirstSetSize.put(aCatAOrB, 1);
+        expectedFirstSetSize.put(a, 1);
+
+        for(Regex expected : expectedFirstSetSize.keySet()){
+
+            for(Regex in : regexInTree){
+                if(expected.isSemanticallyEqual(in)){
+                    assertEquals(expectedFirstSetSize.get(expected), generator.getFirst(in).size());
+                }
+            }
+        }
+
+        // for the next sets
+        // we only print the stuff
+        // seems correct
+        printAllNext(generator, r, 0);
+
+        Map<Regex, Integer> expectedLastSetSize = new HashMap<>();
+        expectedLastSetSize.put(r, 2);
+        expectedLastSetSize.put(aOrb, 2);
+        expectedLastSetSize.put(aCatAOrB, 2);
+        expectedLastSetSize.put(a, 1);
+
+        for(Regex expected : expectedLastSetSize.keySet()){
+
+            for(Regex in : regexInTree){
+                if(expected.isSemanticallyEqual(in)){
+                    assertEquals(expectedLastSetSize.get(expected), generator.getLast(in).size());
+                }
+            }
+        }
+    }
+
+    private void printAllNext(AutomatonGenerator g, Regex r, int level){
+
+        String indent = "\t".repeat(level);
+
+        System.out.printf(indent + "For %s (id: %d): %s\n", r, g.rToId.get(r), g.getNext(r));
+
+        if(r instanceof  Regex.Letter || r instanceof Regex.Empty){
+            // no op, already done
+        }
+        else if(r instanceof Regex.Cat cat){
+
+            System.out.println(indent + "Going left...");
+            printAllNext(g, cat.left, level + 1);
+            System.out.println(indent + "Going right...");
+            printAllNext(g, cat.right, level + 1);
+        }
+        else if(r instanceof Regex.Or or){
+            System.out.println(indent + "Going left...");
+            printAllNext(g, or.left, level + 1);
+            System.out.println(indent + "Going right...");
+            printAllNext(g, or.right, level + 1);
+        }
+        else if(r instanceof Regex.Star star){
+            System.out.println(indent + "Going down...");
+            printAllNext(g, star.r, level + 1);
+        }
+        else if(r instanceof Regex.Grouping grouping){
+            System.out.println(indent + "Going down...");
+            printAllNext(g, grouping.r, level + 1);
+        }
+        else {
+            throw new RuntimeException("Unknown regex type");
         }
     }
 }
